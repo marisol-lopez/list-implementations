9afe84484f82d45c4cee461effc5732cdbdaf930
Marisol Lopez <lmarisolopez@gmail.com>
2017-04-27 11:32:45 -0700

    change

--- lib/array_list.rb
+++ lib/array_list.rb
@@ -2,26 +2,47 @@
 
 class ArrayList
   def initialize
-    @storage = []
+    @storage = [0,0,0,0,0,0,0,0,0,0] #native array: fixed, size, indexing ONLY
+    @size = 0
   end
 
   # Adds _value_ at the end of the list
   def add(value)
+    @storage[@size] = value
+    @size += 1
   end
 
   # Deletes the _last_ value in the array
   def delete
+    raise "bad things" if @size == 0
+    @size -= 1
   end
 
   def include?(key)
+    @size.times do |i|
+      return true if @storage[i] == key
+    end
+    return false
   end
 
   def size
+    return @size
   end
 
   def max
+    biggest = @storage[0]
+    @size.times do |i|
+      if @storage[i] > biggest
+        biggest = @storage[i]
+      end
+    end
+    return biggest
   end
 
   def to_s
+    str = ""
+    @size.times do |i|
+      str += "#{@storage[i]}"
+    end
   end
 end
